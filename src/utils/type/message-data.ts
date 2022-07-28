c3450473d8588af9299b64e02ba8d477725f7d54
Ihor Naidonov <igornaydenov246@gmail.com>
2022-07-28 16:43:07 +0300

    minor fix

--- src/utils/type/message-data.ts
+++ src/utils/type/message-data.ts
@@ -1,9 +1,9 @@
-import { RandomlyGeneratedMessageTypes, UserActionMessageTypes } from '../enums/message-types';
+import { ServerActionMessageTypes, UserActionMessageTypes } from '../enums/message-types';
 
 import { Message } from './message';
 import { User } from './user';
 
 export type MessageData = {
-  type: RandomlyGeneratedMessageTypes | UserActionMessageTypes;
+  type: ServerActionMessageTypes | UserActionMessageTypes;
   data: Message | User | Date | string;
 };
