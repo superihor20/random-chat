0d8c38e8ccc8f825dfd8851cca9a1f1193316696
Ihor Naidonov <igornaydenov246@gmail.com>
2022-08-01 11:29:57 +0300

    added color to the user

--- src/utils/data/users-data.ts
+++ src/utils/data/users-data.ts
@@ -1,482 +1,604 @@
-export const users = [
+import { Users } from '../type/user';
+
+export const users: Users = [
   {
     id: 0,
     username: 'seanjwk',
+    color: 'rgba(137,12,125)',
   },
   {
     id: 1,
     username: 'furqanriaz',
+    color: 'rgba(111,25,241)',
   },
   {
     id: 2,
     username: 'microshanks',
+    color: 'rgba(239,187,2)',
   },
   {
     id: 3,
     username: 'captainpogg',
+    color: 'rgba(141,99,57)',
   },
   {
     id: 4,
     username: 'narpwizzy',
+    color: 'rgba(216,97,238)',
   },
   {
     id: 5,
     username: 'lalosalamancareal',
+    color: 'rgba(87,53,155)',
   },
   {
     id: 6,
     username: 'kresos007',
+    color: 'rgba(137,28,80)',
   },
   {
     id: 7,
     username: 'ZapsMhurvicc',
+    color: 'rgba(107,56,217)',
   },
   {
     id: 8,
     username: 'phreeezy',
+    color: 'rgba(78,120,139)',
   },
   {
     id: 9,
     username: 'SendPizza_',
+    color: 'rgba(153,188,212)',
   },
   {
     id: 10,
     username: 'DRIXX081',
+    color: 'rgba(158,226,183)',
   },
   {
     id: 11,
     username: 'SlXXXXXX',
+    color: 'rgba(65,25,133)',
   },
   {
     id: 12,
     username: 'thatsofaking',
+    color: 'rgba(116,213,56)',
   },
   {
     id: 13,
     username: 'ChiefBret',
+    color: 'rgba(29,39,163)',
   },
   {
     id: 14,
     username: 'Ahpll',
+    color: 'rgba(113,59,13)',
   },
   {
     id: 15,
     username: 'gamedemon010',
+    color: 'rgba(159,113,219)',
   },
   {
     id: 16,
     username: 'SLBori01',
+    color: 'rgba(69,177,100)',
   },
   {
     id: 17,
     username: 'qWerTybored',
+    color: 'rgba(181,192,148)',
   },
   {
     id: 18,
     username: 'thraxexus',
+    color: 'rgba(244,200,80)',
   },
   {
     id: 19,
     username: 'LXXIVthMockingjay',
+    color: 'rgba(182,224,53)',
   },
   {
     id: 20,
     username: 'themidnightbill',
+    color: 'rgba(76,49,198)',
   },
   {
     id: 21,
     username: 'Fossabot',
+    color: 'rgba(208,11,132)',
   },
   {
     id: 22,
     username: 'slumpsquatch',
+    color: 'rgba(117,56,47)',
   },
   {
     id: 23,
     username: 'epettis',
+    color: 'rgba(191,229,112)',
   },
   {
     id: 24,
     username: 'chadman2020',
+    color: 'rgba(206,106,118)',
   },
   {
     id: 25,
     username: 'P1utoFM',
+    color: 'rgba(177,213,99)',
   },
   {
     id: 26,
     username: 'iseedudpeople',
+    color: 'rgba(199,25,70)',
   },
   {
     id: 27,
     username: 'dreamzeo',
+    color: 'rgba(19,126,118)',
   },
   {
     id: 28,
     username: 'Smortest',
+    color: 'rgba(233,216,34)',
   },
   {
     id: 29,
     username: 'ebo_n',
+    color: 'rgba(110,212,42)',
   },
   {
     id: 30,
     username: 'wowsoitwasyou',
+    color: 'rgba(30,75,166)',
   },
   {
     id: 31,
     username: 'Celtion',
+    color: 'rgba(178,44,178)',
   },
   {
     id: 32,
     username: 'halfskeed',
+    color: 'rgba(137,217,48)',
   },
   {
     id: 33,
     username: 'bionicgrey',
+    color: 'rgba(64,33,62)',
   },
   {
     id: 34,
     username: 'brokwifii',
+    color: 'rgba(6,5,225)',
   },
   {
     id: 35,
     username: 'TheyCallMeCoolio',
+    color: 'rgba(37,149,42)',
   },
   {
     id: 36,
     username: 'kleen_pillows',
+    color: 'rgba(18,27,208)',
   },
   {
     id: 37,
     username: 'Sarahhhh',
+    color: 'rgba(145,164,95)',
   },
   {
     id: 38,
     username: 'airsXO',
+    color: 'rgba(8,77,168)',
   },
   {
     id: 39,
     username: 'ClickHereForContent',
+    color: 'rgba(105,234,74)',
   },
   {
     id: 40,
     username: 'UhhhBrandon',
+    color: 'rgba(145,234,49)',
   },
   {
     id: 41,
     username: 'meatgrind89',
+    color: 'rgba(5,87,251)',
   },
   {
     id: 42,
     username: 'drov_',
+    color: 'rgba(38,58,188)',
   },
   {
     id: 43,
     username: 'FroyTheBoy',
+    color: 'rgba(246,118,182)',
   },
   {
     id: 44,
     username: '5FiveLives5',
+    color: 'rgba(231,79,173)',
   },
   {
     id: 45,
     username: 'ruinknows',
+    color: 'rgba(2,63,248)',
   },
   {
     id: 46,
     username: 'GooberShmoo',
+    color: 'rgba(114,226,71)',
   },
   {
     id: 47,
     username: 'mustachio62',
+    color: 'rgba(134,135,202)',
   },
   {
     id: 48,
     username: 'steriosteve2',
+    color: 'rgba(76,162,203)',
   },
   {
     id: 49,
     username: 'jackksenpai',
+    color: 'rgba(254,114,192)',
   },
   {
     id: 50,
     username: 'vedxdd',
+    color: 'rgba(175,56,237)',
   },
   {
     id: 51,
     username: 'Integrities',
+    color: 'rgba(50,46,118)',
   },
   {
     id: 52,
     username: 'Lundii',
+    color: 'rgba(163,69,87)',
   },
   {
     id: 53,
     username: 'nicotisse',
+    color: 'rgba(25,136,176)',
   },
   {
     id: 54,
     username: 'vertzVAL',
+    color: 'rgba(22,97,245)',
   },
   {
     id: 55,
     username: 'xsc4rsx',
+    color: 'rgba(185,146,240)',
   },
   {
     id: 56,
     username: 'galileo4k',
+    color: 'rgba(179,74,237)',
   },
   {
     id: 57,
     username: 'LeGeneralDan',
+    color: 'rgba(79,29,104)',
   },
   {
     id: 58,
     username: 'WarDawwg',
+    color: 'rgba(53,38,89)',
   },
   {
     id: 59,
     username: 'sa0p0p',
+    color: 'rgba(183,230,130)',
   },
   {
     id: 60,
     username: 'CHOPvIII',
+    color: 'rgba(108,19,146)',
   },
   {
     id: 61,
     username: 'Replying to @sa0p0p',
+    color: 'rgba(160,132,22)',
   },
   {
     id: 62,
     username: 'minhthsp',
+    color: 'rgba(165,96,214)',
   },
   {
     id: 63,
     username: 'ThatBoyAZA',
+    color: 'rgba(40,121,129)',
   },
   {
     id: 64,
     username: 'treytreyvibin',
+    color: 'rgba(139,185,39)',
   },
   {
     id: 65,
     username: 'anothrrhit',
+    color: 'rgba(33,75,52)',
   },
   {
     id: 66,
     username: 'dd752',
+    color: 'rgba(167,253,87)',
   },
   {
     id: 67,
     username: 'txr8x',
+    color: 'rgba(54,38,118)',
   },
   {
     id: 68,
     username: 'gemberly',
+    color: 'rgba(29,133,76)',
   },
   {
     id: 69,
     username: 'DylanThVillain',
+    color: 'rgba(174,175,27)',
   },
   {
     id: 70,
     username: 'Skyymiless',
+    color: 'rgba(21,222,19)',
   },
   {
     id: 71,
     username: 'THIZZBOX707',
+    color: 'rgba(70,171,221)',
   },
   {
     id: 72,
     username: 'MR_HK747',
+    color: 'rgba(156,174,75)',
   },
   {
     id: 73,
     username: 'ixlys',
+    color: 'rgba(10,46,133)',
   },
   {
     id: 74,
     username: 'SnacKeeeZ',
+    color: 'rgba(193,214,26)',
   },
   {
     id: 75,
     username: 'Yoshiro_PL',
+    color: 'rgba(12,230,187)',
   },
   {
     id: 76,
     username: 'ZullXv',
+    color: 'rgba(167,0,152)',
   },
   {
     id: 77,
     username: 'feeds904',
+    color: 'rgba(56,54,17)',
   },
   {
     id: 78,
     username: 'jayoliveraa',
+    color: 'rgba(150,60,142)',
   },
   {
     id: 79,
     username: 'Fossabot ',
+    color: 'rgba(169,133,80)',
   },
   {
     id: 80,
     username: 'f34r1t',
+    color: 'rgba(13,54,60)',
   },
   {
     id: 81,
     username: 'lemonnnd',
+    color: 'rgba(88,226,75)',
   },
   {
     id: 82,
     username: 'scubbar1',
+    color: 'rgba(153,85,146)',
   },
   {
     id: 83,
     username: 'prominence50',
+    color: 'rgba(172,234,66)',
   },
   {
     id: 84,
     username: 'TokerTy420',
+    color: 'rgba(51,149,179)',
   },
   {
     id: 85,
     username: 'SlayedDragon666',
+    color: 'rgba(192,156,200)',
   },
   {
     id: 86,
     username: 'kiakinetics',
+    color: 'rgba(43,250,59)',
   },
   {
     id: 87,
     username: 'onquiet',
+    color: 'rgba(138,63,123)',
   },
   {
     id: 88,
     username: 'Bunsen1',
+    color: 'rgba(123,197,98)',
   },
   {
     id: 89,
     username: 'J1mmyOwen',
+    color: 'rgba(215,27,233)',
   },
   {
     id: 90,
     username: 'monopolyman001',
+    color: 'rgba(105,16,120)',
   },
   {
     id: 91,
     username: 'yeboistefanlevianto',
+    color: 'rgba(165,120,41)',
   },
   {
     id: 92,
     username: 'Arxxel',
+    color: 'rgba(69,10,236)',
   },
   {
     id: 93,
     username: 'yourboinitro',
+    color: 'rgba(87,132,154)',
   },
   {
     id: 94,
     username: 'abhi_gamer_03',
+    color: 'rgba(63,118,91)',
   },
   {
     id: 95,
     username: 'snacci',
+    color: 'rgba(205,152,137)',
   },
   {
     id: 96,
     username: 'Cheitanda',
+    color: 'rgba(28,254,17)',
   },
   {
     id: 97,
     username: 'ddundunn',
+    color: 'rgba(142,190,101)',
   },
   {
     id: 98,
     username: 'tongys',
+    color: 'rgba(25,59,190)',
   },
   {
     id: 99,
     username: 'KallMeBrogs',
+    color: 'rgba(251,60,150)',
   },
   {
     id: 100,
     username: 'captalnlevi',
+    color: 'rgba(39,30,239)',
   },
   {
     id: 101,
     username: 'SirPlsStop',
+    color: 'rgba(65,178,111)',
   },
   {
     id: 102,
     username: 'sw337',
+    color: 'rgba(212,170,137)',
   },
   {
     id: 103,
     username: 'sandfir1',
+    color: 'rgba(191,145,79)',
   },
   {
     id: 104,
     username: 'toafloast3',
+    color: 'rgba(66,150,47)',
   },
   {
     id: 105,
     username: '0rang3_turd',
+    color: 'rgba(253,152,171)',
   },
   {
     id: 106,
     username: 'DuckoIII',
+    color: 'rgba(182,121,52)',
   },
   {
     id: 107,
     username: 'Louislangs',
+    color: 'rgba(42,142,9)',
   },
   {
     id: 108,
     username: 'BurtMacklinFBI99',
+    color: 'rgba(112,174,149)',
   },
   {
     id: 109,
     username: 'Vaticn_',
+    color: 'rgba(145,72,218)',
   },
   {
     id: 110,
     username: 'Valor_Now',
+    color: 'rgba(142,68,228)',
   },
   {
     id: 111,
     username: 'idondurma',
+    color: 'rgba(214,67,49)',
   },
   {
     id: 112,
     username: 'zaffetaffy',
+    color: 'rgba(78,71,190)',
   },
   {
     id: 113,
     username: 'DERANGEDWARRIOR',
+    color: 'rgba(80,129,102)',
   },
   {
     id: 114,
     username: 'jebbwebb',
+    color: 'rgba(56,141,149)',
   },
   {
     id: 115,
     username: 'chilliwow',
+    color: 'rgba(40,220,180)',
   },
   {
     id: 116,
     username: 'liberatemm',
+    color: 'rgba(250,117,197)',
   },
   {
     id: 117,
     username: 'ry4n_mp4',
+    color: 'rgba(21,197,194)',
   },
   {
     id: 118,
     username: 'HPainthererightnow',
+    color: 'rgba(66,19,137)',
   },
   {
     id: 119,
     username: 'wardy1204',
+    color: 'rgba(193,208,67)',
   },
 ];
