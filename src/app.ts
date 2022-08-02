4edb6dde5fdbcea16d595a2e554912741bc53b25
Ihor Naidonov <igornaydenov246@gmail.com>
2022-08-02 11:02:01 +0300

    added some static info for message

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import RandomMessages from './modules/RandomMessage/RandomMessage.service';
 import { messages } from './utils/data/messages-data';
 import { users } from './utils/data/users-data';
 import { ServerActionMessageTypes, UserActionMessageTypes } from './utils/enums/message-types';
+import getRandomNumber from './utils/helpers/getRandomNumber';
 import { MessageData } from './utils/type/message-data';
 
 config();
@@ -18,6 +19,8 @@ config();
 const intervalTime = 1000 * 1;
 const loggerService = new LoggerService();
 const chat = new RandomMessages(messages, users, loggerService);
+const myColor = `rgba (${getRandomNumber(255)}, ${getRandomNumber(255)}, ${getRandomNumber(255)})`;
+const myId = 400; // TODO: ya potom ybery
 
 const app = express();
 const server = createServer(app);
@@ -39,7 +42,15 @@ io.on('connection', (socket) => {
   socket.on(UserActionMessageTypes.MY_MESSAGE, (message) => {
     const messageData: MessageData = {
       type: ServerActionMessageTypes.CHAT_MESSAGE,
-      data: `Me: ${message}`,
+      data: {
+        id: myId,
+        message,
+        user: {
+          id: new Date().getTime(), // TODO: i eto toje potom ybery
+          username: 'Me',
+          color: myColor,
+        },
+      },
     };
 
     io.emit(ServerActionMessageTypes.CHAT_MESSAGE, messageData);
