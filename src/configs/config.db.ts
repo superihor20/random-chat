011cb4279aa15c408ef40f39480358b8b5a6ad0a
Ihor Naidonov <superihor20@gmail.com>
2022-08-05 11:20:17 +0300

    refactoring

--- src/configs/config.db.ts
+++ src/configs/config.db.ts
@@ -5,8 +5,8 @@ import { DataSource } from 'typeorm';
 import { configDev } from './config.dev';
 
 const absPathToSrc = join(__dirname, '../');
-const entities = join(absPathToSrc, '/entities/*.ts');
-const migrations = join(absPathToSrc, '/migrations/*.ts');
+const absPathToEntities = join(absPathToSrc, '/entities/*.ts');
+const absPathToMigrations = join(absPathToSrc, '/migrations/*.ts');
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -17,6 +17,6 @@ export const AppDataSource = new DataSource({
   database: configDev.dbName,
   logging: true,
   synchronize: false,
-  entities: [entities],
-  migrations: [migrations],
+  entities: [absPathToEntities],
+  migrations: [absPathToMigrations],
 });
--- src/configs/config.dev.ts
+++ src/configs/config.dev.ts
@@ -2,7 +2,7 @@ export const configDev = {
   port: process.env.PORT || 3000,
   clienUrl: process.env.CLIENT_URL || 'http://localhost:4200',
   dbHost: process.env.DB_HOST || 'localhost',
-  dbPort: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+  dbPort: process.env.DB_PORT && !Number.isNaN(+process.env.DB_PORT) ? +process.env.DB_PORT : 5432,
   dbUsername: process.env.DB_USERNAME || 'user',
   dbPassword: process.env.DB_PASSWORD || '123123123',
   dbName: process.env.DB_NAME || 'chat',
