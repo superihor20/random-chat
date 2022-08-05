44514b3229c04935fa6250d443e07fc4535e2b11
Ihor Naidonov <superihor20@gmail.com>
2022-08-05 16:59:18 +0300

    added new helpers

--- src/modules/Auth/Auth.service.ts
+++ src/modules/Auth/Auth.service.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
-import { sign } from 'jsonwebtoken';
 import { SafeParseReturnType } from 'zod';
 
-import { configDev } from '../../configs/config.dev';
 import { User } from '../../entities/User';
+import { generateAccessToken } from '../../utils/helpers/generateAccessToken';
+import { generateRefreshToken } from '../../utils/helpers/generateRefreshToken';
 import { authSchema } from '../../utils/schemas/signUp.schema';
 import { AuthBody, AuthResponse } from '../../utils/types/user';
 import { HttpError } from '../Error/HttpError.class';
@@ -13,8 +13,6 @@ export class AuthService {
   #userService: UserService;
 
   #saltRounds = 10;
-
-  #tokenExpiresIn = 1000 * 60 * 15;
 
   constructor(userService: UserService) {
     this.#userService = userService;
@@ -46,17 +44,8 @@ export class AuthService {
     user.password = await bcrypt.hash(body.password, this.#saltRounds);
 
     const newUser = await this.#userService.saveUser(user);
-    const accessToken = sign(
-      {
-        id: newUser.id,
-        email: newUser.email,
-      },
-      configDev.secret,
-      {
-        expiresIn: this.#tokenExpiresIn,
-      }
-    );
-    const refreshToken = await bcrypt.hash(new Date().toUTCString(), this.#saltRounds);
+    const accessToken = generateAccessToken({ id: newUser.id, email: newUser.email });
+    const refreshToken = await generateRefreshToken(this.#saltRounds);
 
     return {
       accessToken,
@@ -87,17 +76,8 @@ export class AuthService {
       throw new HttpError(403, 'Incorect credentials, please try again', 'Sign In');
     }
 
-    const accessToken = sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      configDev.secret,
-      {
-        expiresIn: this.#tokenExpiresIn,
-      }
-    );
-    const refreshToken = await bcrypt.hash(new Date().toUTCString(), this.#saltRounds);
+    const accessToken = generateAccessToken({ id: user.id, email: user.email });
+    const refreshToken = await generateRefreshToken(this.#saltRounds);
 
     return {
       accessToken,
